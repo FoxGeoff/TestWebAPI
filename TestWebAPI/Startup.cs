b039d0db575ab85800e0b4f991b692a462bf99e2
FoxGeoff <fox.geoffrey@gmail.com>
2019-08-17 19:25:14 -0700

    Add StaticFiles() ref and node command

--- TestWebAPI/Startup.cs
+++ TestWebAPI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,12 @@ namespace TestWebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Added Start************************************
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+            // Added End**************************************
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -41,7 +48,27 @@ namespace TestWebAPI
             }
 
             app.UseHttpsRedirection();
+            // Added Start************************************
+            app.UseStaticFiles();
             app.UseMvc();
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = @"..\ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+
+            /* ******************************************************
+             * Make sure the property "launchUrl": "api/values"
+             * is not present in the Properties/launchSettings.json,
+             * if it is then delete it.
+             * ******************************************************/
+
+            // Added End**************************************
         }
     }
 }
